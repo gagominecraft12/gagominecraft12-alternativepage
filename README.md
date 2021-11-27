924b504eec4c7e7b7f6b389349d9ac190e8f6f09
Blue'ity <65516039+gagominecraft12@users.noreply.github.com>
2021-11-27 13:12:37 -0600

    yes

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-aaa
+# tusa
+
+sussy tusa minion tusa to kil guard
+Credits to the respective owners.
+
+| Contents   | Path                                            |
+| ---------- | ----------------------------------------------- |
+| Visual VDM | <code>\blue'ity client rips-newvideomaker</code>|
+| rbb        | <code>\roblox ban bypasser by blueity</code>    |
+| idk 1      | <code>\idk 1</code>                             |
+||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
---
+++ roblox ban bypasser by blueity/account deleted.js
@@ -0,0 +1,17 @@
+let number = [];
+var badurl = window.location.hostname = "https://web.roblox.com/not-approved";
+var bypassUrl = window.location.replace = "https://roblox.com";
+var expiry_origin_fourm_why = ["https://en.help.roblox.com/"];
+var day = [11, 14];
+var spot_url = ["ftp://roblox.com"];
+let accountdlted = document.getElementById("fieldset");
+accountdlted.style.display = "none";
+accountdlted.textContent = "You're free. rescued by: girlfreind";
+setTimeout(2);
+window.location.href = 'https://roblox.com';
+
+if (badurl === "/not-approved" !== Boolean.apply("1")) {
+    window.location.replace = "https://web.roblox.com" + window.location.protocol("mobile://");
+} else {
+    bypassUrl();
+}
